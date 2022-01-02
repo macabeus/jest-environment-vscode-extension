c5158f11a17b953dfd6e8dd49f4b6f77d64d2957
macabeus <bruno.macabeus@gmail.com>
2022-01-02 02:32:51 +0000

    using-mocks: avoid exception when not using and document an edge case

--- src/usingMocks.ts
+++ src/usingMocks.ts
@@ -1,16 +1,28 @@
+import dedent from 'dedent-js'
 import keys from 'lodash/keys'
 import set from 'lodash/set'
 import get from 'lodash/get'
+import size from 'lodash/size'
 
 type Mocks = { [key: string]: unknown }
 
 const vscodeOriginalProperties: Mocks = {}
 
 const usingVscodeMocks = async (mocks: Mocks, closure: () => Promise<void>) => {
+  if (size(mocks) === 0) {
+    await closure()
+    return
+  }
+
   const globalVscode = globalThis.extensionVscode
 
   if (globalVscode === undefined) {
-    throw new Error('Missing "global.extensionVscode". Check the setup documentation for jest-environment-vscode-extension')
+    throw new Error(dedent(`
+      Missing "global.extensionVscode". We can't mock VSCode.
+      It can happen because of two reasons:
+      - the extension was not initialized
+      - wrong jest-environment-vscode-extension setup
+    `))
   }
 
   const mocksPath = keys(mocks)
